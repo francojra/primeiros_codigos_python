16f772ab7d7687e67a4f8be8300600cbc05c3b7b
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2023-06-02 18:25:28 -0300

    Add files via upload

--- primeiros_codigos.py
+++ primeiros_codigos.py
@@ -10,3 +10,15 @@ print(5 + 3)
 print(8 * 2)
 print(5**3) # 5 elevado a 3
 print(8 / 2)
+
+### Criação de variáveis
+
+a = 4
+b = 6
+
+soma = a + b
+print(soma)
+
+quadrado = soma**2
+print(quadrado)
+
