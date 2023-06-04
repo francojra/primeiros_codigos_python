fdf6fc50765ee78e5fea1db15122d9d9e97bba45
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2023-06-03 23:56:24 -0300

    Add files via upload

--- exercicio1_python_para_iniciantes.py
+++ exercicio1_python_para_iniciantes.py
@@ -2,14 +2,15 @@
 
 ## Etapas iniciais
 
+### O texto precisa estar dentro das aspas.
+
 print("Olá mundo!")
 print("Didática tech")
 print("oi", "tudo bem?")
-
-## O texto precisa estar dentro das aspas.
 
 print(5 + 6)
 print(4 - 2)
 print(3 ** 4)
 print(3 * 7)
 print(9 / 5)
+print(6 - 3)
