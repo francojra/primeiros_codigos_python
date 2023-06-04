7a6286ee09a85fe7c884ee767385585c8294ec65
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2023-06-04 00:34:15 -0300

    Add files via upload

---
+++ vscode_python_para_iniciantes.py
@@ -0,0 +1,24 @@
+# Exercício 1 - Python para iniciantes
+
+### Textos (precisam estar dentro das aspas)
+
+print("Olá mundo!")
+print("Didática tech")
+print("oi", "tudo bem?")
+
+### Operações matemáticas
+
+print(5 + 6)
+print(4 - 2)
+print(3 ** 4)
+print(3 * 7)
+print(9 / 5)
+print(6 - 3)
+print(17 / 3)
+print(5 % 2) # Resto da divisão
+
+### Criando variáveis
+
+a = 5
+print(a)
+a == 3 # Pergunta se a é igual a 5, a resposta é "true" ou "false"
